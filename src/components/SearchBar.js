4cbae237031a2455df1607d7eb9eca59d50fd209
Bruna <brunaalmeida075@gmail.com>
2023-01-30 16:16:14 -0300

    requisito 11 em andamento

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,48 +1,82 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {
+  getMealsByFirstLetter,
+  getMealsByName,
+  getMealsByIngredients } from '../helpers/fetchAPI';
 
 class SearchBar extends React.Component {
+  state = {
+    search: '',
+
+  };
+
+  a = () => {
+    const { search, name, Ingredient, firstLetter } = this.state;
+    const number = 1;
+
+    if (name === true) {
+      getMealsByName(search);
+    } if (Ingredient === true) {
+      getMealsByIngredients(search);
+    } if (firstLetter === true) {
+      search.length > number ? console.log('maior') : console.log('menor');
+      //getMealsByFirstLetter(search);
+      // if (search.length > number) { console.log('maior'); }
+    }
+    //if (search.length > number) { console.log('maior'); }
+  };
+
   render() {
     return (
       <div>
-        <input data-testid="search-input" type="text" placeholder="Search" />
+        <input
+          data-testid="search-input"
+          type="text"
+          name="search"
+          placeholder="Search"
+          onChange={ (e) => this.setState({ [e.target.name]: e.target.value }) }
+        />
         <label htmlFor="ingredient-radio">
-          Ingredient
+
           <input
             data-testid="ingredient-search-radio"
             type="radio"
-            name="search-radio"
+            name="Ingredient"
             value="ingredient"
             id="ingredient-radio"
-            // onChange={ xesque }
+            onChange={ (e) => this.setState({ [e.target.name]: e.target.checked }) }
           />
+          Ingredient
         </label>
         <label htmlFor="name-radio">
-          Name of Recipe
           <input
             data-testid="name-search-radio"
             type="radio"
-            name="search-radio"
+            name="name"
             value="name"
             id="name-radio"
-            // onChange={ xesque }
+            onChange={ (e) => this.setState({ [e.target.name]: e.target.checked }) }
           />
+          Name
         </label>
         <label htmlFor="first-letter-radio">
-          Primeira Letra
           <input
             data-testid="first-letter-search-radio"
             type="radio"
-            name="search-radio"
+            name="firstLetter"
             value="first-letter"
             id="first-letter-radio"
-            // onChange={ xesque }
+            onChange={ (e) => this.setState({ [e.target.name]: e.target.checked }) }
           />
+          First letter
         </label>
 
         <button
           data-testid="exec-search-btn"
           type="button"
-        //   onClick={ handleSearch }
+          onClick={ this.a }
+          onChange={ this.a }
         >
           Buscar
         </button>
@@ -51,4 +85,4 @@ class SearchBar extends React.Component {
   }
 }
 
-export default SearchBar;
+export default connect()(SearchBar);
--- src/helpers/fetchAPI.js
+++ src/helpers/fetchAPI.js
@@ -33,3 +33,20 @@ export const getDrinksByCategory = async (category) => {
   const data = await response.json();
   return data;
 };
+
+export const getMealsByIngredients = async (ingredients) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredients}`);
+  const data = await response.json();
+  return data;
+};
+
+export const getMealsByName = async (name) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
+  const data = await response.json();
+  return data;
+};
+export const getMealsByFirstLetter = async (FirstLetter) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${FirstLetter}`);
+  const data = await response.json();
+  return data;
+};
