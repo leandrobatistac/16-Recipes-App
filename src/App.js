e6d0c41c01486ef8f8c0355f3b88605edbfcdaa4
mikael jefferson da silva <mikaeljeffeson@gmail.com>
2023-02-04 14:20:28 -0300

    tela recipeinprogress

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 import Recipes from './pages/Recipes';
 import Details from './pages/Details';
+import RecipeInProgress from './pages/RecipeInProgress';
 
 function App() {
   return (
     <div>
       <Switch>
+        <Route
+          path="/drinks/:id/in-progress"
+          component={ RecipeInProgress }
+        />
+        <Route
+          path="/meals/:id/in-progress"
+          component={ RecipeInProgress }
+        />
         <Route exact path="/" component={ Login } />
         <Route exact path="/profile" component={ Profile } />
         <Route exact path="/done-recipes" component={ DoneRecipes } />
--- src/helpers/fetchAPI.js
+++ src/helpers/fetchAPI.js
@@ -68,3 +68,15 @@ export const getDrinksByFirstLetter = async (FirstLetter) => {
   const data = await response.json();
   return data;
 };
+
+export const getMealsByID = async (id) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+  const data = await response.json();
+  return data;
+};
+
+export const getDrinksByID = async (id) => {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+  const data = await response.json();
+  return data;
+};
